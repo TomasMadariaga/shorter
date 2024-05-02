ac6c20cfba45f19aac5e2e6c401ca269b9cf90be
TomasMadariaga <tomas.madariaga@municipiodetandil.edu.ar>
2024-05-02 11:35:41 -0300

    database variable edited

--- client/frontend/src/api/axios.js
+++ client/frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "//shorter-production-8fe4.up.railway.app",
+  baseURL: "https://shorter-production-8fe4.up.railway.app",
   withCredentials: true,
   method: 'GET,HEAD,PUT,PATCH,POST,DELETE'
 });
