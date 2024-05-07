be520c899acc03157eefdca4486f19d7b7f9f6e8
TomasMadariaga <tomas.madariaga@municipiodetandil.edu.ar>
2024-05-07 18:36:23 -0300

    origin cors

--- src/app.js
+++ src/app.js
@@ -11,14 +11,8 @@ app.use(cookieParser());
 
 app.use(
   cors({
-<<<<<<< HEAD
-    origin: "https://shortener-production-1fa3.up.railway.app/",
+    origin: "shortener-production-1fa3.up.railway.app",
     credentials: true
-=======
-    origin: ["http://localhost:5173", "https://shortener-frontend-one.vercel.app", "https://shortener-production-1fa3.up.railway.app"],
-    credentials: true,
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
->>>>>>> 0be1e588fb9560ef8d6f4684361b2be3e1b169bb
   })
 );
 
