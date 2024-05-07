98e5acf03b9211667e683f3c33d3b0659ee0e5ee
TomasMadariaga <tomas.madariaga@municipiodetandil.edu.ar>
2024-05-07 18:28:54 -0300

    Cors variables edited

--- src/app.js
+++ src/app.js
@@ -6,17 +6,15 @@ import { authRoutes } from "./routes/auth.routes.js";
 
 export const app = express();
 
-app.use(
-  cors({
-    origin: ["http://localhost:5173", "https://shortener-frontend-one.vercel.app"],
-    credentials: true,
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  })
-);
-
 app.use(express.json());
 app.use(cookieParser());
-// app.use(cors());
+
+app.use(
+  cors({
+    origin: "https://shortener-production-1fa3.up.railway.app/",
+    credentials: true
+  })
+);
 
 app.use("/u", urlRoutes);
 app.use("/auth", authRoutes);
