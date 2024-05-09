5694f53640f08dc0b9c7d444eb0464b55d38a615
TomasMadariaga <tomas.madariaga@municipiodetandil.edu.ar>
2024-05-09 17:24:27 -0300

    added authRequire to logout

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -7,10 +7,11 @@ import {
 } from "../controllers/auth.controller.js";
 import { validateSchema } from "../middlewares/validator.middleware.js";
 import { registerSchema, loginSchema } from "../schemas/auth.schema.js";
+import { authRequired } from "../middlewares/validateToken.js";
 
 export const authRoutes = Router();
 
 authRoutes.post("/register", validateSchema(registerSchema), register);
 authRoutes.post("/login", validateSchema(loginSchema), login);
-authRoutes.post("/logout", logout);
+authRoutes.post("/logout", authRequired, logout);
 authRoutes.get("/verify", verifyToken);
